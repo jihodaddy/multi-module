d4ccb8188d7aefc74f89091021b7f058c8637d3f
jihodaddy <lastboom@gmail.com>
2022-09-22 03:22:51 +0900

    multi-module

---
+++ root/core/src/main/java/com/example/TestService.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public String test(){
+        return "core test!";
+    }
+
+
+}
